d8b2802fc9673300b935341c49ca38a33aeee112
Angeline Arino <angeline.arino@bisu.edu.ph>
2024-11-13 22:08:39 +0800

    evaluation and admin about

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('about/', views.about_view, name='about'),
     path('student_evaluation_form/', views.student_evaluation_form_view, name='student_evaluation_form'),
     path('admin_dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
+    path('admin_about/', views.admin_about_view, name='admin_about'),
     path('logout/', views.logout_view, name='logout'),
 ]
