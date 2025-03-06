b05af06a30b64b1d2d4a00d1d2c245d3283d374a
John Jayford Managaytay <johnjayford.managaytay@bisu.edu.ph>
2025-03-06 16:34:23 +0800

    Added CSRF_TRUSTED_ORIGINS

--- sentiment/settings.py
+++ sentiment/settings.py
@@ -30,6 +30,12 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['senti-guide.onrender.com', 'sentiment-system.onrender.com', '.render.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://senti-guide.onrender.com',
+    'https://sentiment-system.onrender.com',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+]
 
 # Application definition
 
